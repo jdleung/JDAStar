e569af11bde611eab4292af6919ac58f553f4688
jdleung <jdleungs@gmail.com>
2022-05-30 10:18:20 +0800

    Remove Pod folder

--- Example/Pods/Target Support Files/JDAStar-macOS/JDAStar-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JDAStar_macOS : NSObject
-@end
-@implementation PodsDummy_JDAStar_macOS
-@end
--- Example/Pods/Target Support Files/Pods-JDAStar_Tests/Pods-JDAStar_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JDAStar_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_JDAStar_Tests
-@end
